93c0d6fbc29e3c82eca347535215e572bd920088
Sergey Nezhigay <abc1971h@gmail.com>
2022-11-29 14:59:28 +0200

    init

---
+++ src/js/slick.js
@@ -0,0 +1,9 @@
+$('.one-time').slick({
+  prevArrow: '<button type="button" class="my-slick-prev"></button>',
+  nextArrow: '<button type="button" class="my-slick-next"></button>',
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  adaptiveHeight: true,
+  variableWidth: true,
+});
